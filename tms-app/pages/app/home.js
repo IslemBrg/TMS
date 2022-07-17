86aac9dc4903e8472c049fc0b182c2c3a592686f
Islem Bargaoui <slash4games.yt@gmail.com>
2022-07-17 18:46:07 +0100

    home page update
    added welcome page with user info displayed

--- tms-app/pages/app/home.js
+++ tms-app/pages/app/home.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 
 const isLoggedIn = async () => {    
     const router = useRouter()
   const res = await fetch(`http://localhost:3000/api/authentication/authed`)
     const user = await res.json()
-    if ((user == 403) || (user == 401)) {router.push('/Sign/Login')}
+    return(user)
   }
 
 export default function home() {
+  const [FirstName, setFirstName] = useState("")
+  const [LastName, setLastName] = useState("")
+  const [Service, setService] = useState("")
+
     const router = useRouter()
-    isLoggedIn()
+
+    isLoggedIn().then(data => {
+      setFirstName(data[0].FirstName)
+      setLastName(data[0].LastName)
+      setService(data[0].service)
+    })
 
     const handleLogout = async (event) => {
         const res = await fetch('http://localhost:3000/api/authentication/logout')
@@ -21,10 +31,12 @@ export default function home() {
     }
   return (
     <div>
-        <div>home</div>
         <Button color='success' onClick={handleLogout}>
             <b style={{fontSize: "1.7vh"}}>   Logout</b>
         </Button>
+        <h1>bienvenu</h1><br/>
+        <h2>M./Mme   {FirstName} {LastName}</h2>
+        <h2>Service : {Service}</h2>
     </div>
   )
 }
